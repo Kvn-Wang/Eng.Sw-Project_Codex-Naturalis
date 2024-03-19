95aefa277f5e614ace664ce0d2636074b053506d
Kvn-Wang1 <kevinwang9000@mail.polimi.it>
2024-03-19 11:28:16 +0100

    Creazione Progetto Java base

--- CodexNaturalis_AM47/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module it.polimi.codexnaturalis_am47 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens it.polimi.codexnaturalis_am47 to javafx.fxml;
-    exports it.polimi.codexnaturalis_am47;
-}
