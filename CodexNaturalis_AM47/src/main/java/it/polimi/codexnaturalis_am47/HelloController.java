09be10da6429fd4bfd64e30bf0921343f59296de
Kvn-Wang1 <kevinwang9000@mail.polimi.it>
2024-03-19 11:27:08 +0100

    Creazione Progetto Java base

---
+++ CodexNaturalis_AM47/src/main/java/it/polimi/codexnaturalis_am47/HelloController.java
@@ -0,0 +1,14 @@
+package it.polimi.codexnaturalis_am47;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ CodexNaturalis_AM47/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module it.polimi.codexnaturalis_am47 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens it.polimi.codexnaturalis_am47 to javafx.fxml;
+    exports it.polimi.codexnaturalis_am47;
+}
