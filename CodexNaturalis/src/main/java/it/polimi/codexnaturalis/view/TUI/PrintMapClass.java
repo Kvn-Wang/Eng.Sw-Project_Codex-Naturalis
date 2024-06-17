360988d3537f845ca7c8efb9b75cbf450b5a3994
Knacken8 <77000998+Knacken8@users.noreply.github.com>
2024-06-17 23:11:56 +0200

    tui card class and mission

--- CodexNaturalis/src/main/java/it/polimi/codexnaturalis/view/TUI/PrintMapClass.java
+++ CodexNaturalis/src/main/java/it/polimi/codexnaturalis/view/TUI/PrintMapClass.java
@@ -1,13 +1,39 @@
 package it.polimi.codexnaturalis.view.TUI;
 
+import it.polimi.codexnaturalis.model.enumeration.ResourceType;
 import it.polimi.codexnaturalis.model.player.GamePlayerMap;
+import it.polimi.codexnaturalis.model.player.PlayerScoreResource;
+import it.polimi.codexnaturalis.model.shop.card.Card;
+import it.polimi.codexnaturalis.model.shop.card.StarterCard;
+import it.polimi.codexnaturalis.utils.UtilCostantValue;
 
 public class PrintMapClass {
     public static void main(String[] args) {
-
+        String s ="╔══════╦═══════╦══════╗";
+        int f = s.length();
+        System.out.println("forees"+f);
     }
 
     public static void printMap(GamePlayerMap map){
+        String s ="╔══════╦═══════╦══════╗";
+        int f = s.length();
+        System.out.println(f);
+    }
 
+    public static int[] firstPrintableCardPos(Card[][] mapArray){
+        int i=0, j=0;
+        Card card = null;
+        while(card == null){
+            card = mapArray[i][j];
+            i++;
+            if(i >= UtilCostantValue.lunghezzaMaxMappa){
+                i=0;
+                j++;
+            }
+            if(j >= UtilCostantValue.lunghezzaMaxMappa){
+                j=0;
+            }
+        }
+        return new int[]{i-1,j};
     }
 }
