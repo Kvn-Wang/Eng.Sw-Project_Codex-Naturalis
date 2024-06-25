270c3c94db7259f94a2439e72b1895816bc0e09e
Federico Mondelli <161482632+3Mari@users.noreply.github.com>
2024-06-25 18:36:48 +0200

    implement chat test

---
+++ CodexNaturalis/src/test/java/it/polimi/codexnaturalis/model/chat/ChatManagerTest.java
@@ -0,0 +1,24 @@
+package it.polimi.codexnaturalis.model.chat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChatManagerTest {
+    private ChatManager chatManager;
+
+    @BeforeEach
+    public void setUp() {
+        chatManager = new ChatManager();
+    }
+
+    @Test
+    public void testWriteCommentWithRecipient() {
+
+    }
+
+    @Test
+    public void testWriteCommentWithoutRecipient() {
+
+    }
+}
