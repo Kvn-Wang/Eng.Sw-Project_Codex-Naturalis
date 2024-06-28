ab597bb83a2f0a1f3a95981a6ca5af2f09cb4897
Knacken8 <77000998+Knacken8@users.noreply.github.com>
2024-06-28 03:17:56 +0200

    tests

--- CodexNaturalis/src/test/java/it/polimi/codexnaturalis/model/shop/card/ResourceCardTest.java
+++ CodexNaturalis/src/test/java/it/polimi/codexnaturalis/model/shop/card/ResourceCardTest.java
@@ -1,8 +1,6 @@
 package it.polimi.codexnaturalis.model.shop.card;
 
-import it.polimi.codexnaturalis.model.enumeration.ResourceType;
-import it.polimi.codexnaturalis.model.enumeration.CardCorner;
-import it.polimi.codexnaturalis.model.enumeration.CardType;
+import it.polimi.codexnaturalis.model.enumeration.*;
 import it.polimi.codexnaturalis.model.enumeration.ResourceType;
 import it.polimi.codexnaturalis.model.player.PlayerScoreResource;
 import org.junit.jupiter.api.Test;
@@ -14,15 +12,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class ResourceCardTest {
     @Test
     public void testResourceCard(){
-        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, null, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 0);
+        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, ResourceType.UNASSIGNABLE, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 0);
         assertEquals(CardType.RESOURCE, firstTestCard.getCardType());
     }
 
     @Test
     public void testGetCardResource(){
-        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, null, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 0);
+        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, ResourceType.PLANT, ResourceType.ANIMAL, ResourceType.FUNGI, ResourceType.FUNGI, 0);
         ArrayList<ResourceType> temp = new ArrayList<ResourceType>();
         temp.add(ResourceType.FUNGI);
+        temp.add(ResourceType.PLANT);
+        temp.add(ResourceType.ANIMAL);
         temp.add(ResourceType.FUNGI);
         assertEquals(temp, firstTestCard.getCardResources());
         temp.clear();
@@ -34,9 +34,39 @@ class ResourceCardTest {
     @Test
     public void testGetCardPoints(){
         PlayerScoreResource testScore = null;
-        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, null, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 1);
+        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, ResourceType.UNASSIGNABLE, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 1);
         assertEquals(1, firstTestCard.getCardPoints(testScore, 0));
         firstTestCard.setIsBack(true);
         assertEquals(0, firstTestCard.getCardPoints(testScore, 0));
+    }
+
+    @Test
+    public void testGetFrontCardPoint(){
+        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, ResourceType.UNASSIGNABLE, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 0);
+        assertEquals(0, firstTestCard.getFrontCardPoint());
+    }
+
+    @Test
+    public void testGetBackCentralResource(){
+        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, ResourceType.UNASSIGNABLE, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 1);
+        assertEquals(ResourceType.FUNGI,firstTestCard.getBackCentralResources()[0]);
+    }
+
+    @Test
+    public void testGetCondition(){
+        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, ResourceType.UNASSIGNABLE, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 1);
+        assertEquals(ConditionResourceType.NONE, firstTestCard.getCondition());
+    }
+
+    @Test
+    public void testGetPlaceableCardResources(){
+        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, ResourceType.UNASSIGNABLE, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 1);
+        assertEquals(null, firstTestCard.getPlaceableCardResources());
+    }
+
+    @Test
+    public void testGetFrontalNumber(){
+        ResourceCard firstTestCard = new ResourceCard(1, ResourceType.FUNGI, ResourceType.UNASSIGNABLE, ResourceType.NONE, ResourceType.FUNGI, ResourceType.FUNGI, 1);
+        assertEquals(1, firstTestCard.getFrontalNumber());
     }
 }
--- CodexNaturalis/src/test/java/it/polimi/codexnaturalis/model/shop/card/StarterCardTest.java
+++ CodexNaturalis/src/test/java/it/polimi/codexnaturalis/model/shop/card/StarterCardTest.java
@@ -2,6 +2,7 @@ package it.polimi.codexnaturalis.model.shop.card;
 
 import it.polimi.codexnaturalis.model.enumeration.CardCorner;
 import it.polimi.codexnaturalis.model.enumeration.CardType;
+import it.polimi.codexnaturalis.model.enumeration.ConditionResourceType;
 import it.polimi.codexnaturalis.model.enumeration.ResourceType;
 import org.junit.jupiter.api.Test;
 
@@ -22,8 +23,10 @@ class StarterCardTest {
     @Test
     public void testGetCardResources(){
         int i = 0;
-        StarterCard starterCard = new StarterCard(81, ResourceType.NONE, ResourceType.NONE, ResourceType.PLANT, ResourceType.INSECT, new ResourceType[]{ResourceType.INSECT}, ResourceType.FUNGI, ResourceType.ANIMAL, ResourceType.PLANT, ResourceType.INSECT);
+        StarterCard starterCard = new StarterCard(81, ResourceType.FUNGI, ResourceType.ANIMAL, ResourceType.PLANT, ResourceType.INSECT, new ResourceType[]{ResourceType.INSECT}, ResourceType.FUNGI, ResourceType.ANIMAL, ResourceType.PLANT, ResourceType.INSECT);
         ArrayList<ResourceType> temp = new ArrayList<ResourceType>();
+        temp.add(ResourceType.FUNGI);
+        temp.add(ResourceType.ANIMAL);
         temp.add(ResourceType.PLANT);
         temp.add(ResourceType.INSECT);
         temp.add(ResourceType.INSECT);
@@ -35,5 +38,29 @@ class StarterCardTest {
         temp.add(ResourceType.INSECT);
         temp.add(ResourceType.PLANT);
         assertEquals(temp, starterCard.getCardResources());
+    }
+
+    @Test
+    public void testGetBackCentralResources(){
+        StarterCard starterCard = new StarterCard(81, ResourceType.NONE, ResourceType.NONE, ResourceType.PLANT, ResourceType.INSECT, new ResourceType[]{ResourceType.INSECT}, ResourceType.FUNGI, ResourceType.ANIMAL, ResourceType.PLANT, ResourceType.INSECT);
+        assertEquals(ResourceType.INSECT, starterCard.getBackCentralResources()[0]);
+    }
+
+    @Test
+    public void testGetCondition(){
+        StarterCard starterCard = new StarterCard(81, ResourceType.NONE, ResourceType.NONE, ResourceType.PLANT, ResourceType.INSECT, new ResourceType[]{ResourceType.INSECT}, ResourceType.FUNGI, ResourceType.ANIMAL, ResourceType.PLANT, ResourceType.INSECT);
+        assertEquals(ConditionResourceType.NONE, starterCard.getCondition());
+    }
+
+    @Test
+    public void testGetPlaceableCardResources(){
+        StarterCard starterCard = new StarterCard(81, ResourceType.NONE, ResourceType.NONE, ResourceType.PLANT, ResourceType.INSECT, new ResourceType[]{ResourceType.INSECT}, ResourceType.FUNGI, ResourceType.ANIMAL, ResourceType.PLANT, ResourceType.INSECT);
+        assertEquals(null, starterCard.getPlaceableCardResources());
+    }
+
+    @Test
+    public void testGetFrontalNumber(){
+        StarterCard starterCard = new StarterCard(81, ResourceType.NONE, ResourceType.NONE, ResourceType.PLANT, ResourceType.INSECT, new ResourceType[]{ResourceType.INSECT}, ResourceType.FUNGI, ResourceType.ANIMAL, ResourceType.PLANT, ResourceType.INSECT);
+        assertEquals(0, starterCard.getFrontalNumber());
     }
 }
